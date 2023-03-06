5eae9b52a2cfeaf3a4f09735c7127d9d3376b495
kanemototomoki <kanemototomoki@gmail.com>
2023-03-06 18:32:40 +0900

    finish

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,6 +1,7 @@
 import type { D1Database } from '@cloudflare/workers-types';
 import { handle } from 'hono/cloudflare-pages';
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 type Bindings = {
   DB: D1Database;
@@ -10,12 +11,33 @@ type Todo = {
   title: string;
 };
 
+export type TodoResponse = Todo & {
+  id: number;
+};
+
 const app = new Hono<{ Bindings: Bindings }>();
+
+if (import.meta?.env?.DEV) {
+  app.use(
+    '*',
+    cors({
+      origin: '*',
+      allowHeaders: ['Content-Type', 'Authorization'],
+      allowMethods: ['POST', 'GET', 'OPTIONS'],
+      exposeHeaders: ['Content-Length'],
+      maxAge: 600,
+      credentials: true,
+    })
+  );
+  app.options('*', (c) => {
+    return c.text('', 204);
+  });
+}
 
 const route = app.get('/todos', async (c) => {
   const { results } = await c.env.DB.prepare(
     'SELECT * FROM todos;'
-  ).all<Todo>();
+  ).all<TodoResponse>();
   return c.jsonT(results || []);
 });
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,33 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import { hc } from 'hono/client';
-import type { AppType } from '../functions/api/[[route]]';
+import type { AppType, TodoResponse } from '../functions/api/[[route]]';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState<TodoResponse[]>([]);
   useEffect(() => {
     async function getTodos() {
-      const client = hc<AppType>('/api');
+      const url = import.meta.env.DEV ? 'http://localhost:8788/api' : 'api';
+      const client = hc<AppType>(url);
       const res = await client.todos.$get();
       const json = await res.json();
       return json;
     }
-    const todos = getTodos();
-    console.warn({ todos });
+    getTodos().then((todos) => {
+      console.warn(todos);
+      setTodos(todos);
+    });
   }, []);
 
   return (
     <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      {todos.map((todo) => {
+        return (
+          <p key={todo.id}>
+            id: {todo.id} title: {todo.title}
+          </p>
+        );
+      })}
     </div>
   );
 }
